fd2f2d23fbdda61ff8bc66e83b7f0ba5080abedc
Administrador <viniciuscorrea417@gmail.com>
2024-02-20 00:01:40 -0300

    project done!

--- main.py
+++ main.py
@@ -75,6 +75,6 @@ model.add(Dense(len(trainY[0]), activation='softmax'))
 sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(trainX, trainY, epochs=200, batch_size=5, verbose=1)
+model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.model')
 print('Done')
